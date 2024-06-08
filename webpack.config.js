2d3f10a64d21cbdf5b4cc2a758bccab6275e5d2e
Ross Miller <rossnmiller@gmail.com>
2024-06-08 16:31:22 -0400

    Added npm run clean and added config options to Wasm plugin

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,35 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin(),
         new WasmPackPlugin({
-            crateDirectory: path.resolve(__dirname, ".")
+            crateDirectory: path.resolve(__dirname, "."),
+            // Optional space delimited arguments to appear before the wasm-pack
+            // command. Default arguments are `--verbose`.
+            args: '--log-level warn',
+            // Default arguments are `--typescript --target browser --mode normal`.
+            extraArgs: '--no-typescript',
+            // Optional array of absolute paths to directories, changes to which
+            // will trigger the build.
+            // watchDirectories: [
+            //     path.resolve(__dirname, "src")
+            // ],
+            // The same as the `--out-dir` option for `wasm-pack`
+            outDir: "pkg",
+            // The same as the `--out-name` option for `wasm-pack`
+            outName: "index",
+            // If defined, `forceWatch` will force activate/deactivate watch mode for `.rs` files.
+            // The default (not set) aligns watch mode for `.rs` files to Webpack's
+            // watch mode.
+            forceWatch: true,
+            // If defined, `forceMode` will force the compilation mode for `wasm-pack`
+            //
+            // Possible values are `development` and `production`.
+            //
+            // the mode `development` makes `wasm-pack` build in `debug` mode.
+            // the mode `production` makes `wasm-pack` build in `release` mode.
+            forceMode: "development",
+            // Controls plugin output verbosity, either 'info' or 'error'.
+            // Defaults to 'info'.
+            pluginLogLevel: 'info'
         }),
         // Have this example work in Edge which doesn't ship `TextEncoder` or
         // `TextDecoder` at this time.
