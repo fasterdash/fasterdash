edc9aba9be7814866a87c63070be6b1f00c7f1ff
Michael Trevino <mtrevin93@gmail.com>
2024-06-08 17:46:00 -0400

    not working base_order_by

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-import { greet } from './pkg/index.js';
-greet();
+import { base_order_by } from './pkg/index.js';
+
+const objToSort = [
+  {"name": "John", "age": 30},
+  {"name": "Jane", "age": 25},
+  {"name": "Doe", "age": 50}
+];
+
+console.log(objToSort);
+
+const sortByName = base_order_by(objToSort, "name");
+const sortByAge = base_order_by(objToSort, "age");
+
+console.log({sortByName});
+console.log({sortByAge});
