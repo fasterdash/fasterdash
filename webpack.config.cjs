a9bbcb3caf76d3395a974d027034ef26a491c906
Ross Miller <rossnmiller@gmail.com>
2024-06-09 17:02:10 -0400

    Change to production build for rust

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -39,10 +39,10 @@ module.exports = {
             // //
             // // the mode `development` makes `wasm-pack` build in `debug` mode.
             // // the mode `production` makes `wasm-pack` build in `release` mode.
-            // forceMode: "development",
+            forceMode: "production",
             // // Controls plugin output verbosity, either 'info' or 'error'.
             // // Defaults to 'info'.
-            pluginLogLevel: 'verbose'
+            // pluginLogLevel: 'verbose'
         }),
         // Have this example work in Edge which doesn't ship `TextEncoder` or
         // `TextDecoder` at this time.
