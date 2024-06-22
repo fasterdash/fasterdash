ceb222984b5f8de549fea32941e88f20bcf0523b
Ross Miller <rossnmiller@gmail.com>
2024-06-22 18:03:43 -0500

    Change entry point index.js file. Removed deprecated lib/index.js entry point. Removed lodash as dependency

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-import _ from 'lodash'
-import fasterdash from './lib/index.js';
+import * as pkg from '../pkg/index.js';
+const {
+  initialize,
+  compact,
+} = pkg;
 
-fasterdash.initialize();
-
-const generateDataCompact = (size) => {
-    let largeArray = [];
-    for (let i = 0; i < size; i++) {
-      largeArray.push(i % 10 === 0 ? 0 : i);
-    }
-    return largeArray;
-  };
-
-const compactData = generateDataCompact(100);
-
-console.log({lodashCompact: _.compact(compactData)});
-console.log({fasterdashCompact: fasterdash.compact(compactData)});
+module.exports = pkg;
